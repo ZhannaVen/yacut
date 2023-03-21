8c6e98705c8ec8cc730534d27a41b452f2c8ef3b
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-03-21 11:51:11 +0300

    some updates

--- yacut/utils.py
+++
@@ -1,9 +0,0 @@
-import shortuuid
-from .models import URLMap
-
-
-def get_unique_short_id():
-    short_url = shortuuid.ShortUUID().random(length=16)
-    if URLMap.query.filter(short=short_url).first is None:
-        return short_url
-    return get_unique_short_id()
--- yacut/views.py
+++ yacut/views.py
@@ -1,15 +1,17 @@
 from . import app, db
 from .forms import URLMapForm
-from flask import flash, redirect, render_template
-from .messages import URL_EXISTS, SHORT_URL_EXISTS
+from flask import flash, redirect, render_template, abort
 from .models import URLMap
-from .utils import get_unique_short_id
-from settings import SITE
+from http import HTTPStatus
 import shortuuid
 
 
+SHORT_URL_EXISTS = 'Короткая ссылка занята. Придумайте другую или воспользуйтесь предложенной.'
+
+
 def get_unique_short_id():
-    return shortuuid.ShortUUID().random(length=16)
+    short_url = shortuuid.ShortUUID().random(length=16)
+    return short_url
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,35 +19,40 @@ def index_view():
     form = URLMapForm()
     if form.validate_on_submit():
         original_url = form.original.data
-        url = URLMap.query.filter_by(original=original_url).first()
-        if url:
-            flash(URL_EXISTS.format(SITE, url.short))
-            return render_template('url.html', form=form, url=url)
         short_url = form.short.data
         if short_url:
-            url = URLMap.query.filter_by(short=short_url).first()
-            if url:
+            if URLMap.query.filter_by(short=short_url).first() is not None:
                 new_short_url = get_unique_short_id()
-                flash(SHORT_URL_EXISTS.format(SITE, new_short_url))
-                url = URLMap(
+                flash(SHORT_URL_EXISTS)
+                new_url = URLMap(
                     original=form.original.data,
                     short=new_short_url,
                 )
-                db.session.add(url)
+                db.session.add(new_url)
                 db.session.commit()
-                return render_template('url.html', url=url)
+                return render_template('url.html', url=new_short_url, form=form)
+            new_url = URLMap(
+                original=original_url,
+                short=short_url,
+            )
+            db.session.add(new_url)
+            db.session.commit()
+            return render_template('url.html', url=short_url, form=form)
         short_url = get_unique_short_id()
-        url = URLMap(
-            original=form.original.data,
+        new_url = URLMap(
+            original=original_url,
             short=short_url,
         )
-        db.session.add(url)
+        db.session.add(new_url)
         db.session.commit()
-        return render_template('url.html', url=url)
+        return render_template('url.html', url=short_url, form=form)
     return render_template('url.html', form=form)
 
 
 @app.route('/<string:short>')
 def redirect_view(short):
     url = URLMap.query.filter_by(short=short).first()
+    if url is None:
+        abort(HTTPStatus.NOT_FOUND)
     return redirect(url.original, 301)
+
